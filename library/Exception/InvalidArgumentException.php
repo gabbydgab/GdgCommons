803b43649f3595ba863160760bd9333bb8bfa07f
gamba <gamba@gabbydgab.com>
2014-06-12 03:38:48 -0400

    added exception classes

---
+++ library/Exception/InvalidArgumentException.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is a private property of US Auto Parts Network 
+ * (an E-commerce company & Internet Retailer of  affordable & high quality 
+ * automotive and motorcycle parts and accessories). 
+ * 
+ * This file is subject to the terms & conditions 
+ * of the US Auto Parts confidentiality rules. 
+ * 
+ * Distribution and copying without proper authorization of this file is illegal
+ * 
+ * @copyright (c) 2013 - 2014, US Auto Parts Network
+ * @link http://www.usautoparts.net
+ * @license http://www.usautoparts.net/license
+ */
+
+namespace GdgCommons\Exception;
+
+/**
+ * GdgCommons\Exception\InvalidArgumentException
+ *
+ * @author Gab Amba <gamba@gabbydgab.com>
+ * @package GdgCommons\Exception
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements AwareInterface
+{
+    
+}
